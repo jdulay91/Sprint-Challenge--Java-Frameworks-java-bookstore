fa1ac91d3fa2ad89877952aacc3be73b1a6baf1a
Julliann Dulay <jullianndulay@gmail.com>
2020-09-18 09:20:00 -0700

    first test passed

--- bookstore/src/test/java/com/lambdaschool/bookstore/BookstoreApplication.java
+++ bookstore/src/test/java/com/lambdaschool/bookstore/BookstoreApplication.java
@@ -4,11 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
  * Main class to start the application.
  */
-// @EnableJpaAuditing
+//@EnableJpaAuditing
 @SpringBootApplication
 public class BookstoreApplication
 {
